76321780075909966b2cbd0a76d74cdcd48b00e8
birgitbs <birgitbs@stud.ntnu.no>
2021-03-19 15:28:02 +0100

    States

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.mygdx.game.JavelinGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = JavelinGame.WIDTH;
+		config.height = JavelinGame.HEIGHT;
+		config.title = JavelinGame.TITLE;
+
+
 		new LwjglApplication(new JavelinGame(), config);
 	}
 }
