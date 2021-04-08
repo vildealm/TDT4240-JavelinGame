c9bcf6b2cc81d6db72aaaed21e9d62f8343b40d4
birgitbs <birgitbs@stud.ntnu.no>
2021-04-08 13:32:05 +0200

    Fresh new start with states

--- core/src/com/mygdx/game/model/states/MenuState.java
+++ core/src/com/mygdx/game/model/states/MenuState.java
@@ -3,58 +3,43 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MenuState extends State {
     private BitmapFont font;
-    TextButton startButton;
-    TextButton.TextButtonStyle textButtonStyle;
-    Skin skin;
-    TextureAtlas buttonAtlas;
-    Stage stage;
+    //TextButton startButton;
+    //TextButton.TextButtonStyle textButtonStyle;
+    //Skin skin;
+    //TextureAtlas buttonAtlas;
+    //Stage stage;
 
 
     public MenuState(GameStateManager gsm){
         super(gsm);
-        stage = new Stage();
+        //stage = new Stage();
         font = new BitmapFont();
-        textButtonStyle = new TextButton.TextButtonStyle();
-        skin = new Skin();
-        buttonAtlas = new TextureAtlas("../android/assetsHeliSprites.png");
-        skin.addRegions(buttonAtlas);
-        textButtonStyle.fontColor = new Color(0f, 1f, 1f, 1f);
-        textButtonStyle.font = font;
+     //   textButtonStyle = new TextButton.TextButtonStyle();
+        //skin = new Skin();
+       // buttonAtlas = new TextureAtlas("../android/assetsHeliSprites.png");
+       // skin.addRegions(buttonAtlas);
+        //textButtonStyle.fontColor = new Color(0f, 1f, 1f, 1f);
+       // textButtonStyle.font = font;
         //textButtonStyle.up = skin.getDrawable("up-button");
         //textButtonStyle.down = skin.getDrawable("down-button");
         //textButtonStyle.checked = skin.getDrawable("checked-button");
-        stage.addActor(startButton);
-
-        startButton = new TextButton("START GAME", textButtonStyle);
-
-        startButton.addListener(new ClickListener(){
-
-            public void clicked(InputEvent event, Actor actor)
-            {
-                System.out.println("You clicked me!");
-            }
-        });
-    }
-
-    public void clickButton() {
+        //stage.addActor(startButton);
 
     }
 
     @Override
     public void handleInput() {
-        if(startButton.isPressed()){
+      /*  if(startButton.isPressed()){
             gsm.set(new GameState(gsm));
+            dispose();
+        }*/
+
+        if(Gdx.input.justTouched()){
+            gsm.set(new GameState(gsm));
+
             dispose();
         }
     }
@@ -62,15 +47,14 @@ public class MenuState extends State {
     @Override
     public void update(float dt) {
         handleInput();
-
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        startButton.draw(sb,1 );
+        font.draw(sb, "MENU STATE", 70, 280);
+      //startButton.draw(sb,1 );
         sb.end();
-
     }
 
     public void dispose(){
