e50ba90de1eaa03434f4d581d2c83c69b1adeb2f
Vilde Almestad <vildealmestad@dhcp-10-220-212-243.wlan.ntnu.no>
2021-04-15 12:48:02 +0200

    added controller

---
+++ core/src/com/mygdx/game/controller/PlayerController.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.controller;
+
+public class PlayerController {
+
+    private String username;
+
+    public void setUsername(String username){
+       this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+}
