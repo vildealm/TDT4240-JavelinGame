47ab8fbbc3af0ba5c6c749486d5a73b5a397e23b
Vilde Almestad <vildealmestad@dhcp-10-220-212-243.wlan.ntnu.no>
2021-04-16 14:12:16 +0200

    clean up

--- core/src/com/mygdx/game/view/MenuScreen.java
+++ core/src/com/mygdx/game/view/MenuScreen.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -14,7 +13,6 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.mygdx.game.model.Assets;
 import com.mygdx.game.model.components.Javelin;
-import com.mygdx.game.model.states.GameState;
 import com.mygdx.game.model.states.GameStateManager;
 import com.mygdx.game.model.states.SetupState;
 
@@ -42,8 +40,6 @@ public class MenuScreen implements Screen2 {
         Gdx.input.setInputProcessor(stage);
         playButtonStyle = new TextButton.TextButtonStyle();
         playButtonStyle.font = font;
-        //stage.addActor(javelin);
-        //stage.addTouchFocus(new InputListener(), javelin, javelin, 1,1);
         font = new BitmapFont();
         TextButton playButton = new TextButton("PLAY", playButtonStyle);
         playButton.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
@@ -60,11 +56,6 @@ public class MenuScreen implements Screen2 {
                gsm.set(new SetupState(gsm));
             }
         });
-
-
-        //background = Assets.getTexture(Assets.menuBackground);
-        //playBtn = Assets.getTexture(Assets.gameScreenButton);
-
     }
 
     public void show() {
@@ -73,9 +64,6 @@ public class MenuScreen implements Screen2 {
 
     public void render(float delta, SpriteBatch sb) {
         sb.begin();
-        font.draw(sb, "MENU", 70, 100);
-        //game.getBatch().draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //Draws background photo
-        //game.getBatch().draw(playBtn, Gdx.graphics.getWidth()/2-playBtn.getWidth()/2, Gdx.graphics.getHeight()/2 );
         sb.draw(setupSprite, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         sb.end();
         stage.act(Gdx.graphics.getDeltaTime());
