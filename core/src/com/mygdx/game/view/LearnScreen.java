8b84b7b73bc07848b9624d49af30c1c8e6a33910
birgitbs <birgitbs@stud.ntnu.no>
2021-04-22 16:16:42 +0200

    learn page done

--- core/src/com/mygdx/game/view/LearnScreen.java
+++ core/src/com/mygdx/game/view/LearnScreen.java
@@ -7,12 +7,17 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.game.model.Assets;
 import com.mygdx.game.model.states.GameStateManager;
-
-
+import com.mygdx.game.model.states.MenuState;
+import com.mygdx.game.model.states.MultiplayerSelectionState;
 
 
 public class LearnScreen implements Screen2 {
@@ -22,6 +27,9 @@ public class LearnScreen implements Screen2 {
     private Sprite learnSprite;
     private BitmapFont font;
     private BitmapFont font2;
+    private Texture backImage;
+    private Button backButton;
+
 
 
     public LearnScreen(final GameStateManager gsm){
@@ -29,8 +37,26 @@ public class LearnScreen implements Screen2 {
         learnTexture = (Assets.getTexture(Assets.learnImage));
         learnSprite = new Sprite(Assets.getTexture(Assets.learnImage));
         stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
         font = new BitmapFont();
         font2 = new BitmapFont();
+        backImage = Assets.getTexture(Assets.backButton);
+        backButton = new Button(new TextureRegionDrawable(new TextureRegion(backImage)));
+        backButton.setHeight(90);
+        backButton.setWidth(350);
+        backButton.setPosition(20, 650);
+        stage.addActor(backButton);
+
+        backButton.addListener(new ChangeListener(){
+            @Override
+            public void changed(ChangeEvent event, Actor actor){
+                gsm.set(new MenuState(gsm));
+            }
+        });
+
+
+
     }
 
 
@@ -50,10 +76,11 @@ public class LearnScreen implements Screen2 {
         font2.draw(sb, "High speed ->  high score" , (Gdx.graphics.getWidth() /2 ) -150, 70);
         font2.draw(sb, "Close to the line ->  high score" , (Gdx.graphics.getWidth() /2 ) -2000, 30);
 
-
         font.getData().setScale(2.5f);
         font2.getData().setScale(2f);
         sb.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 
     }
 
